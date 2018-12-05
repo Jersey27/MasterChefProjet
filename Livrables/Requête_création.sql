ea6e1c304d6e540d6fd2a5b67fb85e5e37cf40d0
Bryan <bryan.quatremare@viacesi.Fr>
2018-12-05 10:45:02 +0100

    Ajout des scripts de remplissage et de modification de la BDD
    Ajout du script de remplissage et modification du script de création

--- Livrables/Requête_création.sql
+++ Livrables/Requête_création.sql
@@ -1,39 +1,40 @@
-
 DROP TABLE IF EXISTS Ingredients ;
 CREATE TABLE Ingredients (id_ingredient INT IDENTITY NOT NULL,
 nom_ingredient VARCHAR(255),
-quantite_ingredient_recette BIGINT,
-type_ingredient VARCHAR(50) NOT NULL CHECK (type_ingredient IN('Frais', 'Surgelé', 'Longue date de conservation')), 
+type_ingredient VARCHAR(50) NOT NULL CHECK (type_ingredient IN('Frais','Surgelé', 'Longue conserv')),
+quantite_ingredient TINYINT,
 date_peremption_ingredient DATE,
-PRIMARY KEY (id_ingredient));  
+PRIMARY KEY (id_ingredient));
 
 DROP TABLE IF EXISTS Materiel ;
 CREATE TABLE Materiel (id_materiel INT IDENTITY NOT NULL,
-nom_materiel VARCHAR,
-type_materiel VARCHAR(50) NOT NULL CHECK (type_materiel IN('Matériel commun', 'Matériel de restauration', 'Matériel de cuisine')),
-etat_materiel VARCHAR(50) NOT NULL CHECK (etat_materiel IN('Sale', 'Propre', 'En machine')),
+nom_materiel VARCHAR(255),
+type_materiel VARCHAR(50) NOT NULL CHECK (type_materiel IN('Matériel commun','Matériel de restauration', 'Matériel de cuisine')),
+etat_materiel VARCHAR(50) NOT NULL CHECK (etat_materiel IN('Propre','Sale', 'En machine')),
 quantite_materiel TINYINT,
-PRIMARY KEY (id_materiel)); 
+PRIMARY KEY (id_materiel));
 
 DROP TABLE IF EXISTS Machines ;
 CREATE TABLE Machines (id_machine INT IDENTITY NOT NULL,
-nom_machine TEXT,
-etat_machine VARCHAR(50) NOT NULL CHECK (etat_machine IN('Vide', 'Plein', 'Allumé')),
-PRIMARY KEY (id_machine)); 
+nom_machine VARCHAR(255),
+etat_machine VARCHAR(50) NOT NULL CHECK (etat_machine IN('Allumé','Vide', 'Plein')),
+PRIMARY KEY (id_machine));
 
 DROP TABLE IF EXISTS Recettes ;
 CREATE TABLE Recettes (id_recette INT IDENTITY NOT NULL,
-nom_recette TEXT,
+nom_recette VARCHAR(255),
 temps_cuisson_recette SMALLINT,
 temps_repos_recette SMALLINT,
 temps_preparation_recette SMALLINT,
-PRIMARY KEY (id_recette)); 
+PRIMARY KEY (id_recette));
 
-DROP TABLE IF EXISTS LISTER ; 
-CREATE TABLE LISTER (id_ingredient INT IDENTITY NOT NULL,
-id_recette INT NOT NULL,
+DROP TABLE IF EXISTS Composition ;
+CREATE TABLE Composition (id_composition INT IDENTITY NOT NULL,
 quantite_ingredient_recette BIGINT,
-PRIMARY KEY (id_ingredient,  id_recette));
+id_ingredient INT,
+id_recette INT,
+PRIMARY KEY (id_composition));
 
-ALTER TABLE LISTER ADD CONSTRAINT FK_LISTER_id_ingredient FOREIGN KEY (id_ingredient) REFERENCES Ingredients (id_ingredient);
-ALTER TABLE LISTER ADD CONSTRAINT FK_LISTER_id_recette FOREIGN KEY (id_recette) REFERENCES Recettes (id_recette); 
+ALTER TABLE Composition ADD CONSTRAINT FK_Composition_id_ingredient FOREIGN KEY (id_ingredient) REFERENCES Ingredients (id_ingredient);
+
+ALTER TABLE Composition ADD CONSTRAINT FK_Composition_id_recette FOREIGN KEY (id_recette) REFERENCES Recettes (id_recette);
