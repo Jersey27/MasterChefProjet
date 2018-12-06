2dc3f43c814d5d779f27a0cbd49a4b27097618c1
Unknown <marandlycee@gmail.com>
2018-12-06 16:25:34 +0100

    Création du projet de Tests Unitaires

--- MasterChefResto/MasterChefResto/model/MasterHotel.cs
+++ MasterChefResto/MasterChefResto/model/MasterHotel.cs
@@ -8,19 +8,20 @@ namespace MasterChefResto.model
 {
     class MasterHotel
     {
-        public void Sum()
+        private int money;
+        public void Sum(int monet)
         {
-
+            this.money = this.money + monet;
         }
 
         public void SearchTable()
         {
-
+            //si une table est reconnue comme libre et possède un nombre suffisant de place (choix de la plus petite correspondant à cette dernière contrainte).
         }
 
         public void AttributeTable()
         {
-
+            //Donne une valeur particulière au client pour le diriger vers la table choisie
         }
     }
 }
---
+++ MasterChefResto/MasterChefRestoTest/MasterHotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MasterChefResto.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterChefRestoTest
+{
+    [TestClass]
+    public class MasterHotel
+    {
+  
+        [TestMethod]
+        public void sumTest()
+        {
+            int somme = 10;
+            int monet = 5;
+            somme = somme + monet;
+            //MasterHotel.Sum;
+            //Assert.IsTrue();
+        }
+    }
+}
