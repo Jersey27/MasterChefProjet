e03b7e852d45fab17caac4e07c07292f4370b5df
Unknown <tristan.lefevre@viacesi.fr>
2018-12-07 09:56:25 +0100

    ajout d'une base de données fake
    Pour les tests unitaires

---
+++ MasterChefCuisine/BDDFalse/dbo/Tables/Recettes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Recettes (id_recette INT IDENTITY NOT NULL,
+nom_recette VARCHAR(255),
+temps_cuisson_recette SMALLINT,
+temps_repos_recette SMALLINT,
+temps_preparation_recette SMALLINT,
+PRIMARY KEY (id_recette));
