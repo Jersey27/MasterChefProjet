6d5a0350904bc0287f9ef80277e347508e4c2d9d
PimontJr <baptiste.pimont@viacesi.fr>
2018-12-11 11:14:41 +0100

    debut client socket

--- MasterChefResto/MasterChefResto/model/SocketManagement.cs
+++ MasterChefResto/MasterChefResto/model/SocketManagement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace MasterChefResto.model
 {
@@ -12,6 +14,24 @@ namespace MasterChefResto.model
 
         private SocketManagement()
         {
+            bool next = true;
+            IPAddress ip = IPAddress.Parse("");
+            int port = 12345;
+            IPEndPoint ipEnd = new IPEndPoint(ip, port);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                client.Connect(ipEnd);
+            }
+            catch (SocketException E)
+            {
+                Console.WriteLine("Connection" + E.Message);
+            }
+            while (next)
+            {
+
+            }
 
         }
 
