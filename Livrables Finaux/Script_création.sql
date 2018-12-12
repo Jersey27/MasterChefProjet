40d9803405a481e22edd15d7e7770f852706984f
Bryan <bryan.quatremare@viacesi.Fr>
2018-12-12 13:32:09 +0100

    Correction des erreurs dans les scripts SQL

--- Livrables Finaux/Script_création.sql
+++ Livrables Finaux/Script_création.sql
@@ -1,11 +1,12 @@
 use masterchef;
+DROP TABLE IF EXISTS Composition_Recette ;
 DROP TABLE IF EXISTS Ingredients ;
 CREATE TABLE Ingredients (id_ingredient INT IDENTITY NOT NULL,
 nom_ingredient VARCHAR(255) NOT NULL,
 type_ingredient VARCHAR(20) NOT NULL CHECK (type_ingredient IN('Frais','Surgelé', 'Longue conserv')),
-quantite_ingredient TINYINT NOT NULL,
+quantite_ingredient INT NOT NULL,
 date_peremption_ingredient DATE,
-ingredient_preparable BOOLEAN,
+ingredient_preparable BIT,
 PRIMARY KEY (id_ingredient));
 
 DROP TABLE IF EXISTS Materiel ;
@@ -13,7 +14,7 @@ CREATE TABLE Materiel (id_materiel INT IDENTITY NOT NULL,
 nom_materiel VARCHAR(255) NOT NULL,
 type_materiel VARCHAR(30) NOT NULL CHECK (type_materiel IN('Matériel commun','Matériel de restauration', 'Matériel de cuisine')),
 etat_materiel VARCHAR(15) NOT NULL CHECK (etat_materiel IN('Propre','Sale', 'En machine')),
-quantite_materiel TINYINT NOT NULL,
+quantite_materiel INT NOT NULL,
 PRIMARY KEY (id_materiel));
 
 DROP TABLE IF EXISTS Machines ;
@@ -25,14 +26,14 @@ PRIMARY KEY (id_machine));
 DROP TABLE IF EXISTS Recettes ;
 CREATE TABLE Recettes (id_recette INT IDENTITY NOT NULL,
 nom_recette VARCHAR(255) NOT NULL,
-temps_cuisson_recette INT NOT NULL,
-temps_repos_recette SMALLINT NOT NULL,
-temps_preparation_recette SMALLINT NOT NULL,
+temps_cuisson_recette SMALLINT,
+temps_repos_recette SMALLINT,
+temps_preparation_recette SMALLINT,
 nombre_parts SMALLINT NOT NULL,
 type_recette VARCHAR(10) NOT NULL CHECK (type_recette IN('Entrée','Plat', 'Dessert')),
 PRIMARY KEY (id_recette));
 
-DROP TABLE IF EXISTS Composition_Recette ;
+
 CREATE TABLE Composition_Recette (id_composition_recette INT IDENTITY NOT NULL,
 quantite_ingredient_recette INT NOT NULL,
 id_ingredient INT NOT NULL,
