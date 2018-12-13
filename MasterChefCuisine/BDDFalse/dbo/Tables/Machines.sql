82755d8e860e5fcc794cd771e2836b1c5f24e2ee
Bryan <bryan.quatremare@viacesi.Fr>
2018-12-13 15:48:46 +0100

    Suppression de la BDDFake sur le projet de la cuisine

--- MasterChefCuisine/BDDFalse/dbo/Tables/Machines.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE Machines (id_machine INT IDENTITY NOT NULL,
-nom_machine VARCHAR(255) NOT NULL,
-etat_machine VARCHAR(50) NOT NULL CHECK (etat_machine IN('Allumé','Vide', 'Plein')),
-PRIMARY KEY (id_machine));
